82fdd65731c40b56dfef6f79694d3e03f208e445
aks0709 <ayushkumarjgm@gmail.com>
2025-07-24 20:59:09 +0530

    Started  with Lambda Expressions!

---
+++ src/Functional_Interface/FI_DEMO.java
@@ -0,0 +1,28 @@
+package Functional_Interface;
+@FunctionalInterface   //we use annotation to make sure that the interface will have only 1 method
+interface A{
+    void fun();
+
+    //void sum();
+    String toString();  // it doesnt throw error as this method is already overridden and it is method from Object class
+}
+class B implements A{
+    public void fun(){
+        System.out.println("Hey There");
+    }
+}
+
+
+public class FI_DEMO {
+    public static void main(String[] args) {
+        A obj=()->{
+                System.out.println("HEY I AM UNDER ANONYMOUS INNER CLASS");
+            System.out.println("THIS PRINT IS TO TEST MULTIPLE STATEMENT UNDER LAMBDA EXPRESSION BLOCK");
+        };
+
+        A obj2=()-> System.out.println("hELLO I AM SINGLE  LINE STATEMENT");
+        obj.fun();
+        obj2.fun();
+
+    }
+}
